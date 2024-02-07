e267783b02c53e45db32c0217d4a80373306acd9
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-02-07 14:19:02 +0000

    addedEnemies

--- FloorPlatform.lua
+++ FloorPlatform.lua
@@ -14,3 +14,16 @@ end
 function FloorPlatform:draw()
     love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
 end
+
+
+function Enemies:checkBoundries()
+    if self.x < 0 then 
+        self.x = 0
+    elseif self.x + self.width > love.graphics.getWidth() then 
+        self.x = love.graphics.getWidth() - self.width
+    elseif self.y < 0 then 
+        self.y = 0
+    elseif self.y + self.height > love.graphics.getHeight() - 35 then
+        self.y = love.graphics.getHeight() - self.height - 35
+    end 
+end
--- main.lua
+++ main.lua
@@ -1,15 +1,21 @@
 -- this is a comment
 require("Player")
 require("FloorPlatform")
+require("Enemies")
 
 function love.load()
     Player:load()
     FloorPlatform:load()
+    Enemies:load()
+    
 end
+
+
 
 function love.update(dt)
     Player:update(dt)
     FloorPlatform:update(dt)
+    Enemies:update(dt)
 
     for i = #bullets, 1, -1 do
         local bullet = bullets[i]
@@ -25,6 +31,7 @@ end
 function love.draw()
     Player:draw()
     FloorPlatform:draw()
+    Enemies:draw()
     for _, bullet in ipairs(bullets) do
         love.graphics.rectangle("fill", bullet.x, bullet.y, bullet.width, bullet.height)
     end
