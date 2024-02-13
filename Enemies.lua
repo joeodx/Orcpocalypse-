20dc2bdaa5e42601b48497449b35b1ba7720894a
James Robertson <jaarobertson92@gmail.com>
2024-02-13 14:28:55 +0000

    commented out player

--- Enemies.lua
+++ Enemies.lua
@@ -1,26 +1,26 @@
-Enemies {}
+-- Enemies {}
 
 
 
-function Enemies:new()
-    -- gravity = 1500
-    -- jumpHeight = 200
-    self.x = love.graphics.getWidth() - 50
-    self.y = love.graphics.getHeight() - 35
-    self.width = 20
-    self.height = 100
-    self.speed = 500
-    -- 1 for right, -1 for left
-    self.direction = -1
+-- function Enemies:new()
+--     -- gravity = 1500
+--     -- jumpHeight = 200
+--     self.x = love.graphics.getWidth() - 50
+--     self.y = love.graphics.getHeight() - 35
+--     self.width = 20
+--     self.height = 100
+--     self.speed = 500
+--     -- 1 for right, -1 for left
+--     self.direction = -1
 
-end
+-- end
 
-function Enemies:update(dt)
-    self:checkBoundries()
-end
+-- function Enemies:update(dt)
+--     self:checkBoundries()
+-- end
 
-function Enemies:draw()
-    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-end
+-- function Enemies:draw()
+--     love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+-- end
 
     
--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
 -- this is a comment
 require("Player")
 require("FloorPlatform")
-require("Enemies")
+-- require("Enemies")
 
 function love.load()
     Player:load()
     FloorPlatform:load()
-    Enemies:load()
+    -- Enemies:load()
     
 end
 
@@ -15,7 +15,7 @@ end
 function love.update(dt)
     Player:update(dt)
     FloorPlatform:update(dt)
-    Enemies:update(dt)
+    -- Enemies:update(dt)
 
     for i = #bullets, 1, -1 do
         local bullet = bullets[i]
@@ -31,7 +31,7 @@ end
 function love.draw()
     Player:draw()
     FloorPlatform:draw()
-    Enemies:draw()
+    -- Enemies:draw()
     for _, bullet in ipairs(bullets) do
         love.graphics.rectangle("fill", bullet.x, bullet.y, bullet.width, bullet.height)
     end
